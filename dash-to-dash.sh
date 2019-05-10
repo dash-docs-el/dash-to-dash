c2a417255e8bcd352d4c445a2d7f00d5e64a88ab
Raimon Grau <raimonster@gmail.com>
2019-05-10 20:15:28 +0100

    wrap all objects in a big fat array

--- dash-to-dash.sh
+++ dash-to-dash.sh
@@ -1,2 +1,2 @@
 curl -s http://sanfrancisco.kapeli.com/feeds/zzz/user_contributed/build/index.json  2>/dev/null |
-    jq '.docsets | to_entries[] |{name: .value.name , archive: ["http://newyork.kapeli.com/feeds/zzz/user_contributed/build/" , .key, "/", .value.archive] | join("")}'
+    jq '[.docsets | to_entries[] |{name: .value.name , archive: ["http://newyork.kapeli.com/feeds/zzz/user_contributed/build/" , .key, "/", .value.archive] | join("")}]'
